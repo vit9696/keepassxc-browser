8488d5572221738d83d8490a2fb7b221968f7f6f
varjolintu <sami.vanttinen@protonmail.com>
2022-03-13 08:35:43 +0200

    Prevent flash on popup open with Chromium based browsers

--- keepassxc-browser/popups/popup_functions.js
+++ keepassxc-browser/popups/popup_functions.js
@@ -46,8 +46,15 @@ async function initColorTheme() {
     }
 }
 
+// Sets default popup size for Chromium based browsers to prevent flash on popup open
+function setDefaultPopupSize() {
+    if (!isFirefox()) {
+        document.body.style.width = DEFAULT_POPUP_SIZE;
+    }
+}
+
 // Resizes the popup to the default size if the width is too small
-async function resizePopup() {
+function resizePopup() {
     if (document.body.offsetWidth > 0 && document.body.offsetWidth < 180) {
         document.body.style.width = isFirefox() ? PINNED_POPUP_SIZE : DEFAULT_POPUP_SIZE;
     } else {
